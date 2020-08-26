a5950935694f647e143f82894da70c11872c6cb3
attiliogiglio <attilio.g93@gmail.com>
2020-08-26 13:35:16 -0400

    still not working correctly dropdown selector

--- App.js
+++ App.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo")
 
 // Event listeners
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(e){
@@ -48,4 +50,31 @@ function deleteCheck(e){
         const todo2 = item.parentElement;
         todo2.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    console.log(e);
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if(todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }
+                else{
+                    todo.style.display = 'none'
+                }
+            break;
+            // case 'uncompleted':
+            //     if(!todo.classList.contains('uncompleted')){
+            //         todo.style.display = 'flex';
+            //     }
+            //     else{
+            //         todo.style.display = 'none'
+            //     }
+        }
+    })
 }
