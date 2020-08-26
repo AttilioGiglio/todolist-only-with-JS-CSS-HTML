7e994538ae3ffc9d142fce69bc1872acb0fc63b4
attiliogiglio <attilio.g93@gmail.com>
2020-08-26 12:23:01 -0400

    It's done the input submit todo's into todolist

--- App.js
+++ App.js
@@ -15,19 +15,21 @@ function addTodo(event){
     todoDiv.classList.add('todo');
     // Todo li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // Checkbox mark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add('todo-button');
+    completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton); 
     // Cross delete button
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-    deleteButton.classList.add('todo-button');
+    deleteButton.classList.add('delete-btn');
     todoDiv.appendChild(deleteButton);
     // Append to List
-    todoList.appendChild(todoDiv) 
+    todoList.appendChild(todoDiv);
+    // Clear todo input value
+    todoInput.value = ''; 
 }
